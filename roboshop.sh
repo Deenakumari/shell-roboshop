9a054421e96ffc27f361c40442c7a7247bc6aacf
Deenakumari <deenamadhumanchi@gmail.com>
2025-05-26 20:25:45 +0530

    special variables

---
+++ roboshop.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+AMI_ID="ami-09c813fb71547fc4f"
+SG_ID="sg-00a872585424ecb33"
+INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "frontend")
+ZONE_ID="Z10308481ZTCFXVPZ89KX"
+DOMAIN_NAME="deena.fun"
+
+for instance in ${INSTANCES[@]}
+do 
+    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t2.micro --security-group-ids sg-00a872585424ecb33 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=test}]" --query "Instances[0].InstanceId" --output text)
+    if [ $instance != "frontend" ]
+    then
+       IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
+    else
+       IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text) 
+    fi
+    echo "$instance ip address is: $IP" 
+
+
+done
