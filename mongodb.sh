8bab5fa922a8106bdf452fd824d6564853fd2955
Deenakumari <deenamadhumanchi@gmail.com>
2025-05-27 17:14:07 +0530

    special variables

--- mongodb.sh
+++ mongodb.sh
@@ -30,7 +30,7 @@ VALIDATE(){
     fi
 }
 
-cp mongodb.repo /etc/yum.repos.d/mongo.repo
+cp mongo.repo /etc/yum.repos.d/mongo.repo
 VALIDATE $? "copying mongodb repo"
 
 dnf install mongodb-org -y &>>LOG_FILE
